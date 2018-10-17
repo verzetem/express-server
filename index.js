75b2ab54c3cf1cbf52c8440437f3d58e65746948
chris <verzetem@gmail.com>
2018-10-17 11:26:09 -0600

    last commit

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.user(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.get('/', (req, res, next) => {
+	res.send('You are awesome')
+})
 
 app.listen(port, () => {
-	console.log(`I got you on ${port}`)
+	console.log(`I got you on ${ port }`)
 })
